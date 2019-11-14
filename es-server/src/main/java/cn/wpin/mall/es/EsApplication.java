294f081e93e088aa1dfa6da39de74386c4b2ff9c
wangpin <1151749070@qq.com>
2019-11-14 14:20:21 +0800

    update nacos config namespace

--- es-server/src/main/java/cn/wpin/mall/es/EsApplication.java
+++ es-server/src/main/java/cn/wpin/mall/es/EsApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 /**
  * es服务启动类
@@ -13,6 +14,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan(basePackages = "cn.wpin.mall.es")
+@EnableElasticsearchRepositories
 public class EsApplication {
 
     public static void main(String[] args) {
